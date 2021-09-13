5dec6ee456eb29736c614b0a2093a467e06bd408
Michael Nazareth <mcnazareth@gmail.com>
2021-09-13 15:43:51 -0400

    michael contribute

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Beers = () => {
-	return (
-		<div>
-			<h1>Beers</h1>
-		</div>
-	);
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://ih-beers-api2.herokuapp.com/beers`).then((resApi) => {
+      console.log(resApi);
+      setBeers(resApi.data);
+    });
+  }, []);
+  const ShowBeers = () => {
+    return (
+      <ul>
+        {beers.map((beer) => {
+          return <li key={beer._id}>{beer.name}</li>;
+        })}
+      </ul>
+    );
+  };
+  return (
+    <div>
+      <h1>Beers</h1>
+      <ShowBeers />
+    </div>
+  );
 };
 
 export default Beers;
